2e9f9fb4d5a6b95123530e7e93494d2cc1e43fb5
Leandro Lopez <leandrolopez@microsoft.com>
2023-08-26 17:24:56 +0200

    add environmen to push to GitHub

---
+++ Environments/KaaS-WebApp/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+    azurerm = {
+      version = "= 3.53.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+# configure the GitHub provider to use the token stored in the Key Vault
+provider "github" {
+  token = data.azurerm_key_vault_secret.github_token.value
+}
+
+# fetch the GitHub token from the Key Vault
+data "azurerm_key_vault_secret" "github_token" {
+  name         = "github-token"
+  key_vault_id = var.key_vault_id
+}
+
+# fetch the GitHub repository
+data "github_repository" "k8s_state" {
+  full_name = var.k8s_state_repo
+}
+
+# create a file in the GitHub repository
+resource "github_repository_file" "test_file" {
+  repository          = data.github_repository.k8s_state.name
+  branch              = "main"
+  file                = "test"
+  content             = "this is a test"
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Azure Deployment Environments Processor"
+  commit_email        = "ade@microsoft.com"
+}
+
+
+
+
+
+
+
+# data "azurerm_resource_group" "rg" {
+#   name = var.resource_group_name
+# }
---
+++ Environments/KaaS-WebApp/variables.tf
@@ -0,0 +1,17 @@
+variable "k8s_state_repo" {
+  type        = string
+  default     = "lopezleandro03/k8s-state"
+  description = "The name of the GitHub repository to store the Terraform state file in."
+}
+
+variable "key_vault_id" {
+  type        = string
+  description = "The ID of the Azure Key Vault to store the GitHub token in."
+  default     = "/subscriptions/4ad9490b-017e-4a82-8892-39c294f3af9c/resourceGroups/rg-azd-devcenter/providers/Microsoft.KeyVault/vaults/akv-ntawodiyy2yzy"
+}
+
+# variable "resource_group_name" {}
+
+# variable "resource_name" {}
+
+# variable "location" {}
